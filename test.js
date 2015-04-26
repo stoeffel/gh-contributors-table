e448b074ccf95775e55fa7a057e202179bdc4b35
stoeffel <schtoeffel@gmail.com>
2015-04-26 15:02:11 +0200

    add test

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 'use strict';
 var assert = require('assert');
+var fs = require('fs');
 var ghContributorsTable = require('./');
 
-it('should ', function () {
-	assert.strictEqual(ghContributorsTable('unicorns'), 'unicorns & rainbows');
+it('should create a table', function (done) {
+	fs.readFile('./contributors.md', 'utf8', function (err,data) {
+		if (err) {
+			return assert.fail();
+		}
+		ghContributorsTable(['stoeffel', 'stoeffel'], function(err, table) {
+			if (err) {
+				return assert.fail();
+			}
+			assert.strictEqual(table, data);
+			done();
+		});
+	});
 });
